ef959169fe5f021a3fe9c9197c036fa4a6993b04
dannygoodacre <dannygoodacre1@gmail.com>
2022-02-05 23:36:43 +0000

    Begun work on lanczos algorithm function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from methods import *
-from temp import *
 
 A = np.array([[1, 2],[3,4]])
 b = np.array([2,1])
@@ -7,7 +6,7 @@ e1 = np.array([1, 0]).T
 
 #print(V @ matrix_exp_padé(H, 5, 5) @ e1)
 
-V, H = arnoldi(A, b, 1)
+V, H = lanczos(A, b)
 
 with np.printoptions(precision = 3, suppress = True):
     print(np.real(V))
