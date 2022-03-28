970ead09cf805df3c0d0985469c2c84c4c5ad6fa
dannygoodacre <dannygoodacre1@gmail.com>
2022-03-28 15:07:23 +0100

    Generalised expm method for ip, mp, scipy, and me (Gauss-Legendre quad)

--- integrate.py
+++ integrate.py
@@ -15,19 +15,35 @@ def integrate(f, a, b, deg=100):
 
 def pre_integrate(H_coeff, tlist, method):
     integrals = []
-    if method == "scipy":
-        for i in range(len(tlist)):
+    if method == "mag1_scipy":
+        for i in range(len(tlist) - 1):
             val = [0, 0, 0]
-            val[0] = sp.integrate.quad(H_coeff[i][0], tlist[i], tlist[i+1])
-            val[1] = sp.integrate.quad(H_coeff[i][1], tlist[i], tlist[i+1])
+            val[0] = sp.integrate.quad(H_coeff[0], tlist[i], tlist[i+1])[0]
+            val[1] = sp.integrate.quad(H_coeff[1], tlist[i], tlist[i+1])[0]
             val[2] = H_coeff[2] * (tlist[i+1] - tlist[i])
             integrals.append(val)
-    elif method == "me":
-        for i in range(len(tlist)):
+    elif method == "mag1_me":
+        for i in range(len(tlist) - 1):
             val = [0, 0, 0]
-            val[0] = integrate(H_coeff[i][0], tlist[i], tlist[i+1])
-            val[1] = integrate(H_coeff[i][1], tlist[i], tlist[i+1])
+            val[0] = integrate(H_coeff[0], tlist[i], tlist[i+1])
+            val[1] = integrate(H_coeff[1], tlist[i], tlist[i+1])
             val[2] = H_coeff[2] * (tlist[i+1] - tlist[i])
+            integrals.append(val)
+    elif method == "ip":
+        h = tlist[1] - tlist[0]
+        for i in range(len(tlist) - 1):
+            val = [0, 0, 0]
+            val[0] = h * H_coeff[0](tlist[i])
+            val[1] = h * H_coeff[1](tlist[i])
+            val[2] = h * H_coeff[2]
+            integrals.append(val)
+    elif method == "mp":
+        h = tlist[1] - tlist[0]
+        for i in range(len(tlist) - 1):
+            val = [0, 0, 0]
+            val[0] = h * H_coeff[0](tlist[i] + h/2)
+            val[1] = h * H_coeff[1](tlist[i] + h/2)
+            val[2] = h * H_coeff[2]
             integrals.append(val)
     else:
         print("Error: invalid method.")
